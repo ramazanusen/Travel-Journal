d3867d84930eddb50ed511bd118c324ddf9d4a7f
Ramazan Usen <ramazanusen@hotmail.com>
2023-03-24 18:18:56 +0300

    first commit

---
+++ travel_journal/src/Data.js
@@ -0,0 +1,9 @@
+const Data = {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsLink: "www.google.com",
+    startDate: "12 February 2021",
+    endDate: "14 March 2021",
+    description: "lorem ipsum",
+    imageUrl: "www.google.com"
+}
